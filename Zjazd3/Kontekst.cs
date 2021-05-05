a1247d76db120c0b1e671ad0bad74963ea7e9ccb
nelumboo <tk.kielkowski@gmail.com>
2021-05-05 20:28:16 +0200

    Update solves problem with connectionString

--- Zjazd3/Kontekst.cs
+++ Zjazd3/Kontekst.cs
@@ -7,12 +7,18 @@ namespace Zjazd3
 {
     public class Kontekst : DbContext
     {
+        static IConfigurationBuilder  builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: false);
+        static IConfiguration  configuration = builder.Build();
+            
+        string connectionString = configuration.GetConnectionString("myDb1");
+        
+        
         public DbSet<MyUser> MyUsers { get; set; }
         
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(
-                "...");
+                connectionString);
             base.OnConfiguring(optionsBuilder);
         }
     }
--- Zjazd3/Program.cs
+++ Zjazd3/Program.cs
@@ -12,26 +12,24 @@ namespace Zjazd3
       
         static void Main(string[] args)
         {
-            // var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: false);
-            // var configuration = builder.Build();
-            //
-            // string str = configuration.GetConnectionString("myDb1");
 
-            using var db = new ZNorthwindContext();
-            
-            var zm = db.Kliencis.Where(x => x.Kraj.Length>5);
-            
-            foreach (var klienci in zm)
-            {
-                Console.WriteLine(klienci.Idklienta + " " + klienci.Kraj);
+        using var db = new ZNorthwindContext();
+        
+        var zm = db.Kliencis.Where(x => x.Kraj.Length>5);
+        
+        foreach (var klienci in zm)
+        {
+            Console.WriteLine(klienci.Idklienta + " " + klienci.Kraj);
+        
+        }
+        
+        
+        var db2 = new Kontekst();
+        db2.Database.EnsureCreated();
+        
+        db2.MyUsers.Add(new MyUser() {Name = "Jola", RegistrationDate = new DateTime(1985, 12, 31)});
+        db2.SaveChanges();
 
-            }
-
-            var db2 = new Kontekst();
-            db2.Database.EnsureCreated();
-            
-            db2.MyUsers.Add(new MyUser() {Name = "Jola", RegistrationDate = new DateTime(1985, 12, 31)});
-            db2.SaveChanges();
         }
     }
 }
